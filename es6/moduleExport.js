28a629d4c8dec6b146f8616d8c0feb0dfeb4ef66
Renato De la Rosa <rdelarosa@develovers.com.pe>
2022-06-07 20:11:35 -0500

    Modulos

---
+++ es6/moduleExport.js
@@ -0,0 +1,9 @@
+const API_KEY =
+  'jVwBlBSdQhyK06Uvt-vHsw.a95foO08HX_avPjwNv_mg9yg_W7b98d-ZwA6WbS5dWU';
+
+const printMessage = (msg) => {
+  console.log(msg);
+};
+
+//export default API_KEY;
+export { API_KEY, printMessage };
---
+++ es6/moduleImport.js
@@ -0,0 +1,5 @@
+//import API_KEY, { printMessage } from './moduleExport.js';
+import * as Modulos from './moduleExport.js';
+
+Modulos.printMessage('Modulos');
+console.log(Modulos.API_KEY);
